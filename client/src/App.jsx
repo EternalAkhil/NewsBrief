f8ec5244918d4a10d9d454d2e0a55e836fdc6685
EternalAkhil <akr142003@gmail.com>
2025-05-25 10:02:15 +0530

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,12 @@
 import { useState } from 'react'
 
 import './App.css'
-import { Suspense,lazy } from 'react'
-const Home = lazy(()=>import('./pages/Home.jsx'))
-const NewsForm = lazy(()=>import('./components/NewsForm.jsx'))
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from './pages/Home.jsx'
 function App() {
   return (
     <>
    
-    <BrowserRouter>
-    <Suspense fallback={<div className='text-2xl'>Loading...</div>}>
-    <Routes>
-      <Route index path='/' element={<Home/>}/>
-      <Route path='/verify' element={<NewsForm/>}/>
-    </Routes>
-    </Suspense>
-    </BrowserRouter>
+    <Home/>
 
     </>
   )
